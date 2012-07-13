8e463eb4d778276ba59f76d01d2f0bac09e629ed
WebzLogic <webzlogic@windmobile.blackberry.com>
2012-07-12 23:05:46 -0300

    Update master

--- application/models/BlogModel.class.php
+++ application/models/BlogModel.class.php
@@ -9,24 +9,22 @@ class BlogModel extends Model
 	
 	public function Archives()
 	{
-		return "SELECT COUNT(post_ID) AS post_count, MONTHNAME(post_date) AS post_date_month, YEAR(post_date) AS post_date_year
-				FROM test
-				GROUP BY post_date_year, post_date_month
-				ORDER BY post_date";
+		$g = new sql;
+		return $g->SelectFrom("COUNT(post_ID) AS post_count, MONTHNAME(post_date) AS post_date_month, YEAR(post_date) AS post_date_year","test")
+			 ->GroupBy("post_date_year, post_date_month")
+			 ->OrderBy("post_date");
 	}
 	
 	public function GetPosts()
 	{
 		$g = new sql;
-		return $g->SelectFrom(array('post_ID','post_name','post_title','meta_ID'), "hello")
-				 ->ToString();
+		return $g->SelectFrom(array('post_ID','post_name','post_title','meta_ID'), "hello")->ToString();
 	}
 	
 	public function GetPostsByCategory()
 	{
 		$g = new sql;
-		return $g->SelectFrom(array('post_ID','post_name','post_title','meta_ID'), "hello")
-				 ->ToString();
+		return $g->SelectFrom(array('post_ID','post_name','post_title','meta_ID'), "hello")->ToString();
 	}
 }
 
